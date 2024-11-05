563425cc4ff2a23b1c376f706a49253d1d3c703b
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-11-05 21:58:08 +0530

    added all files

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://srinidhikulkarni25:srinidhi7@book-corner.rlazh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Corner';
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import DeleteBooks from './pages/DeleteBooks'
+import DisplayBooks from './pages/DisplayBooks'
+import EditBooks from './pages/EditBooks'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route path='/books/details/:id' element={<DisplayBooks/>} />
+      <Route path='/books/edit/:id' element={<EditBooks/>} />
+      <Route path='/books/delete/:id' element={<DeleteBooks/>} />
+    </Routes>
+  )
+}
+
+export default App
