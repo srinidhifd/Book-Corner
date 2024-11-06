a7af80b95734f9f8cab955c635323b6c73ce8bc8
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-11-06 13:03:18 +0530

    refactored index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import bookRoute from "./routes/booksRoute.js"
 import cors from "cors";
 
