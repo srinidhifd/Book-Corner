a4fa351d4642f7223851210fd21393c5dff54152
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-11-12 16:01:44 +0530

    updated code

--- backend/index.js
+++ backend/index.js
@@ -8,24 +8,24 @@ const app = express();
 
 app.use(express.json());
 
-  
-  app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'], // Configures CORS to allow requests from the specified origins
-    credentials: true // Allows cookies and credentials to be sent with requests
+
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'], // Configures CORS to allow requests from the specified origins
+  credentials: true // Allows cookies and credentials to be sent with requests
 }));
 
 
 
-app.use('/books',bookRoute); 
+app.use('/books', bookRoute);
 
 mongoose.connect(mongoDBURL)
-    .then(() => {
-        console.log("App connected to db");
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`)
-        });
-    })
-    .catch((error) => {
-        console.error(error)
+  .then(() => {
+    console.log("App connected to db");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`)
     });
+  })
+  .catch((error) => {
+    console.error(error)
+  });
 
